6d75230158663b0bc226f6499967fd5f76f338a0
Leandro Moreira <leandro.manhani@gmail.com>
2022-02-17 22:16:47 -0300

    adicionado input

---
+++ src/common/components/Input/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Props } from "./types";
+import { Input } from "@mui/material";
+import { useForm } from "react-hook-form";
+
+
+export function InputStyled(props: Props) {
+    const { 
+        register,
+        formState: { errors },
+    } = useForm();
+    return (
+    <>
+    <input
+        {...register(props.name)}
+        data-test={`input-${props.name}`}
+        type={!!props.type ? props.type : "string"}
+        />
+        <p>{errors[props.name]?.message}</p>
+    </>
+    )
+};
